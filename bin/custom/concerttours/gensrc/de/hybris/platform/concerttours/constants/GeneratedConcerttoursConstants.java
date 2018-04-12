eaf45b5a4c0cd70370d88b2e4ae030b4850f0221
Daniel Richter <daniel.richter@hmmh.de>
2018-04-12 12:48:51 +0200

    extended data model

--- bin/custom/concerttours/gensrc/de/hybris/platform/concerttours/constants/GeneratedConcerttoursConstants.java
+++ bin/custom/concerttours/gensrc/de/hybris/platform/concerttours/constants/GeneratedConcerttoursConstants.java
@@ -1,7 +1,7 @@
 /*
  * ----------------------------------------------------------------
  * --- WARNING: THIS FILE IS GENERATED AND WILL BE OVERWRITTEN! ---
- * --- Generated at 12.04.2018 09:19:18                         ---
+ * --- Generated at 12.04.2018 12:43:15                         ---
  * ----------------------------------------------------------------
  *  
  * [y] hybris Platform
@@ -25,6 +25,42 @@ package de.hybris.platform.concerttours.constants;
 public class GeneratedConcerttoursConstants
 {
 	public static final String EXTENSIONNAME = "concerttours";
+	public static class TC
+	{
+		public static final String BAND = "Band".intern();
+		public static final String CONCERT = "Concert".intern();
+		public static final String CONCERTTYPE = "ConcertType".intern();
+		public static final String MUSICTYPE = "MusicType".intern();
+	}
+	public static class Attributes
+	{
+		public static class MusicType
+		{
+			public static final String BANDS = "bands".intern();
+		}
+		public static class Product
+		{
+			public static final String BAND = "band".intern();
+			public static final String HASHTAG = "hashtag".intern();
+		}
+	}
+	public static class Enumerations
+	{
+		public static class ConcertType
+		{
+			public static final String OPENAIR = "openair".intern();
+			public static final String INDOOR = "indoor".intern();
+		}
+		public static class MusicType
+		{
+			// no values defined
+		}
+	}
+	public static class Relations
+	{
+		public static final String BAND2MUSICTYPE = "Band2MusicType".intern();
+		public static final String PRODUCT2ROCKBAND = "Product2RockBand".intern();
+	}
 	
 	protected GeneratedConcerttoursConstants()
 	{
