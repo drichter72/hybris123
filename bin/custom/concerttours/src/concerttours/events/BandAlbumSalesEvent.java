8bb07432bf2a174dda5b4853098e5d36ffe5b256
Daniel Richter <daniel.richter@hmmh.de>
2018-04-17 09:22:29 +0200

    additional testcase for asyncronous messaging- fails!!

--- bin/custom/concerttours/src/concerttours/events/BandAlbumSalesEvent.java
+++ bin/custom/concerttours/src/concerttours/events/BandAlbumSalesEvent.java
@@ -1,8 +1,9 @@
 package concerttours.events;
 
+import de.hybris.platform.servicelayer.event.ClusterAwareEvent;
 import de.hybris.platform.servicelayer.event.events.AbstractEvent;
 
-public class BandAlbumSalesEvent extends AbstractEvent {
+public class BandAlbumSalesEvent extends AbstractEvent implements ClusterAwareEvent {
 	private final String code;
 	private final String name;
 	private final Long sales;
@@ -29,5 +30,10 @@ public class BandAlbumSalesEvent extends AbstractEvent {
 	@Override
 	public String toString() {
 		return this.name;
+	}
+
+	@Override
+	public boolean publish(final int sourceNodeId, final int targetNodeId) {
+		return (sourceNodeId == targetNodeId);
 	}
 }
